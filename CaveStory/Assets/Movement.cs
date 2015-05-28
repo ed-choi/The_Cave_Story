0c97ca0f92df999009e8db7dc435912d17c297c5
Edward Choi <edward.choi805@gmail.com>
2015-05-27 21:29:18 -0400

    script added

---
+++ CaveStory/Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.LeftArrow))
+			transform.Translate (-speed * Time.deltaTime, 0, 0);
+	}
+}
