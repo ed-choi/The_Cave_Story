4c9e9bcc9ebb7462d9904d8a97ad6a04c8682f15
Shahan Akhter <shahan3.14@gmail.com>
2015-05-29 05:30:14 -0700

    added a LOT to ed's level. did mario mechanics for shiggles. jump on enemies. matt sucks

--- CaveStory/Assets/_Scripts/Player.cs
+++ CaveStory/Assets/_Scripts/Player.cs
@@ -1,45 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (Controller2D))]
-public class Player : MonoBehaviour {
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
 
-	public float jumpHeight = 4;
-	public float timeToJumpApex = .4f;
-	float accelerationTimeAirborne = .2f;
-	float accelerationTimeGrounded = .1f;
-	float moveSpeed = 6;
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .1f;
+    float moveSpeed = 6;
 
-	float gravity;
-	float jumpVelocity;
-	Vector3 velocity;
-	float velocityXSmoothing;
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
 
-	Controller2D controller;
+    Controller2D controller;
 
-	void Start() {
-		controller = GetComponent<Controller2D> ();
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
 
-		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
-		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-		print ("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
-	}
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        print("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
+    }
 
-	void Update() {
+    // Edited original code added simple jump method for mario-like hopping.
+    public void jump()
+    {
+        velocity.y = jumpVelocity;
+    }
 
-		if (controller.collisions.above || controller.collisions.below) {
-			velocity.y = 0;
-		}
+    void Update()
+    {
 
-		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
 
-		if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
-			velocity.y = jumpVelocity;
-		}
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-		float targetVelocityX = input.x * moveSpeed;
-		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
-		velocity.y += gravity * Time.deltaTime;
-		controller.Move (velocity * Time.deltaTime);
-	}
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+        {
+            jump();
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
 }
---
+++ CaveStory/Assets/_Scripts/PlayerStatusHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerStatusHandler : MonoBehaviour
+{
+    public Slider health;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Death or restart whenever.
+        if ((health.value <= 0) || (Input.GetKeyDown(KeyCode.R)))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "tEnemy")
+        {
+            health.value--;
+        }
+    }
+
+}
