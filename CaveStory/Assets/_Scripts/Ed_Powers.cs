e589594a5c2c471c31fbcf0c9d983e606eb2f478
Shahan Akhter <shahan3.14@gmail.com>
2015-05-30 22:12:12 -0700

    added tweaks to Ed-powers, for spring, and stuff. matt sucks

--- CaveStory/Assets/_Scripts/Ed_Powers.cs
+++ CaveStory/Assets/_Scripts/Ed_Powers.cs
@@ -1,77 +1,107 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ed_Powers : MonoBehaviour {
+public class Ed_Powers : MonoBehaviour
+{
 
-	public bool fire, speed, zip, spring, fight, sword;
-	private Player player;
-	private Vector3	playerPos;
-	// Use this for initialization
-	void Start () {
-		player = GetComponent<Player>();
-	}
-	void newPower(int powNum){
-		fire = false;
-		speed = false;
-		zip = false;
-		spring = false;
-		fight = false;
-		sword = false;
-		switch (powNum) {
-		case 0:
-			fire = true;
-			break;
-		case 1:
-			speed = true;
-			break;
-		case 2:
-			zip = true;
-			break;
-		case 3:
-			spring = true;
-			break;
-		case 4:
-			fight = true;
-			break;
-		case 5:
-			sword = true;
-			break;
-		}
-	}
-	// Update is called once per frame
-	void Update () {
-		playerPos = GameObject.Find ("Ed-Player-ARTNOTFINISHED").transform.position;
-		Debug.Log (playerPos);
-		if (fire) {
-		}
-		if (speed) {
-			
-		}
-		if (zip) {
-			if (Input.GetKey (KeyCode.Z)) {
-				if (Input.GetKeyDown (KeyCode.UpArrow)) {
-					Vector3 temp = new Vector3 (0, 5, 0);
-					GameObject.Find ("Ed-Player-ARTNOTFINISHED").transform.position += temp;
-				}
-				if (Input.GetKeyDown (KeyCode.RightArrow)) {
-					Vector3 temp = new Vector3 (5, 0, 0);
-					GameObject.Find ("Ed-Player-ARTNOTFINISHED").transform.position += temp;
-				}
-				if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-					Vector3 temp = new Vector3 (-5, 0, 0);
-					GameObject.Find ("Ed-Player-ARTNOTFINISHED").transform.position += temp;
-				}
-			}
-		}
-		if (spring) {
-			player.setJumpHeight (8);
-		}
-		if (fight) {
-		}
-		if (sword) {
-		}
+    public bool fire, speed, zip, spring, fight, sword;
+    private Player player;
+    private Vector3 playerPos;
+    public bool showPositionInConsole = false; // Tick this to show position in console.
+
+    // For restoring jump height when no spring is true.
+    float originalJumpHeight;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponent<Player>();
+        originalJumpHeight = player.jumpHeight;
+    }
+
+    void newPower(int powNum)
+    {
+        fire = false;
+        speed = false;
+        zip = false;
+        spring = false;
+        fight = false;
+        sword = false;
+        switch (powNum)
+        {
+            case 0:
+                fire = true;
+                break;
+            case 1:
+                speed = true;
+                break;
+            case 2:
+                zip = true;
+                break;
+            case 3:
+                spring = true;
+                break;
+            case 4:
+                fight = true;
+                break;
+            case 5:
+                sword = true;
+                break;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = GameObject.Find("Ed-Player").transform.position;
+
+        if (showPositionInConsole)
+            Debug.Log(playerPos);
+
+        if (fire)
+        {
+        }
+        if (speed)
+        {
+
+        }
+        if (zip)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    Vector3 temp = new Vector3(0, 5, 0);
+                    GameObject.Find("Ed-Player").transform.position += temp;
+                }
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    Vector3 temp = new Vector3(5, 0, 0);
+                    GameObject.Find("Ed-Player").transform.position += temp;
+                }
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    Vector3 temp = new Vector3(-5, 0, 0);
+                    GameObject.Find("Ed-Player").transform.position += temp;
+                }
+            }
+        }
+
+        if (spring)
+        {
+            player.setJumpHeight(8);
+        }
+        else if (!spring)
+        {
+            player.setJumpHeight(originalJumpHeight);
+        }
+
+        if (fight)
+        {
+        }
+        if (sword)
+        {
+        }
 
 
-	
-	}
+    }
 }
