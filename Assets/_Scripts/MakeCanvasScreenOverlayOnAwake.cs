f9d2b4f2d3ba8dcc256e2d71b58d0224f9505f4c
Shahan Akhter <shahan3.14@gmail.com>
2015-06-02 15:46:32 -0700

    added fix for the canvas and editing in scene clicking. matt sucks

---
+++ Assets/_Scripts/MakeCanvasScreenOverlayOnAwake.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeCanvasScreenOverlayOnAwake : MonoBehaviour
+{
+    
+    //  Purely for the fact that the canvas makes it hard to click-select objects that are behind it while
+    //  editing stuff in scene mode/view. So I moved it up 500 unitys and set it to World Space
+    //  in the inspector.
+    void Awake()
+    {
+        this.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+    }
+
+}
