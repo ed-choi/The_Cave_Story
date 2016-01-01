7303ba5f04f873d3605abdb60cf1f4861eed058d
Edward Choi <edward.choi805@gmail.com>
2015-12-31 20:40:35 -0500

    made a test for enemy

---
+++ Assets/Test/enemy.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+    float amplitudeX = 10.0f;
+    float amplitudeY = 5.0f;
+    float omegaX = 1.0f;
+    float omegaY = 5.0f;
+    public float startx;
+    public float starty;
+    float index;
+    public void Update()
+    {
+        index += Time.deltaTime;
+        float x = amplitudeX * Mathf.Cos(omegaX * index);
+        float y = Mathf.Abs(amplitudeY * Mathf.Sin(omegaY * index));
+        transform.localPosition = new Vector3(x+startx, y+starty, 0);
+    }
+    /*private Vector3 left = new Vector3(-1,0,0);
+    private Vector3 up = new Vector3(0, 1, 0);
+    private Vector3 down = new Vector3(0, -1, 0);
+    private float movetime = 1;
+    // Use this for initialization
+    void Start () {
+        Invoke("movementupleft", 0);
+        Invoke("movementdownleft", .1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        left = new Vector3(-1 * Time.deltaTime, 0, 0);
+        up = new Vector3(0, 1*Time.deltaTime, 0);
+        down = new Vector3(0, -1*Time.deltaTime, 0);
+        movetime = Time.deltaTime;
+        //Invoke("moveupleft", 0);
+        //Invoke("movedownleft", 0.01f);
+        //Invoke("movedownleft", 0.02f);
+        //Invoke("moveupleft", 0.03f);
+    }
+    void moveupleft()
+    {
+        transform.position += up;
+        transform.position += left;
+    }
+    void movedownleft()
+    {
+        transform.position += down;
+        transform.position += left;
+    }
+    void movementupleft()
+    {
+        InvokeRepeating("moveupleft", .1f, .1f);
+    }
+    void movementdownleft()
+    {
+        InvokeRepeating("movedownleft", .1f, .1f);
+    }*/
+}
---
+++ Assets/Test/test_spawn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class test_spawn : MonoBehaviour
+{
+    public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 5f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+ 
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
