fb63a8a9cbd025b9ba2a77d87259af4658cde3b1
Edward Choi <edward.choi805@gmail.com>
2015-12-31 22:13:15 -0500

    some more test

---
+++ Assets/Test/enemy2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy2 : MonoBehaviour {
+    public float speed = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveleft = new Vector3(-1 * speed * Time.deltaTime, 0, 0);
+        transform.position += moveleft;
+	}
+}
